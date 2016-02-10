bc5a1dc1711c22155ca27ed007c5da8e4b3a0cd9
Fabiano Rosas <farosas@linux.vnet.ibm.com>
2016-02-10 13:32:30 -0200

    Make tab-completion work inside buffers using dabbrev

--- init.el
+++ init.el
@@ -143,6 +143,22 @@ open and unsaved."
 	  (message "File '%s' successfully renamed to '%s'"
 		   name (file-name-nondirectory new-name)))))))
 
+(defun smart-tab ()
+  "This smart tab is minibuffer compliant: it acts as usual in
+    the minibuffer. Else, if mark is active, indents region. Else if
+    point is at the end of a symbol, expands it. Else indents the
+    current line."
+  (interactive)
+  (if (minibufferp)
+      (unless (minibuffer-complete)
+        (dabbrev-expand nil))
+    (if mark-active
+        (indent-region (region-beginning)
+                       (region-end))
+      (if (looking-at "\\_>")
+          (dabbrev-expand nil)
+        (indent-for-tab-command)))))
+
 (defun uncamelcase ()
   (interactive)
   (progn (replace-regexp "\\([A-Z]\\)" "_\\1" nil (region-beginning)(region-end))
@@ -180,6 +196,7 @@ open and unsaved."
 		(lambda ()
 		  (interactive)
 		  (join-line -1)))
+(global-set-key [(tab)] 'smart-tab)
 
 ;;; Hooks
 ;;---------------------------------------------------------------------------------------
