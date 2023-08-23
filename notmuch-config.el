9ac06057088f24da0bc8d9e69789924d7578e7b9
Fabiano Rosas <fabianorosas@gmail.com>
2023-08-23 15:36:12 -0300

    notmuch helpers

--- notmuch-config.el
+++ notmuch-config.el
@@ -47,6 +47,14 @@
 (setq hscroll-margin 1)
 (setq hscroll-step 1)
 
+(defun link ()
+  (interactive)
+  (notmuch-show-stash-mlarchive-link "Lore"))
+
+(defun id ()
+  (interactive)
+  (notmuch-show-stash-message-id-stripped))
+
 (defun extract-patch (dir)
   (interactive "Ddir: ")
   (let ((id (notmuch-show-get-message-id t)))
@@ -118,6 +126,57 @@
   (goto-char (point-min))
   (while (search-forward "\n" nil t) (replace-match "\r\n")))
 
+(defun mailto-compose-mail (mailto-url)
+  "Parse MAILTO-URL and start composing mail."
+  (if (and (stringp mailto-url)
+           (string-match "\\`mailto:" mailto-url))
+      (progn
+        (require 'rfc2368)
+        (require 'rfc2047)
+        (require 'mailheader)
+
+        (let ((hdr-alist (rfc2368-parse-mailto-url mailto-url))
+              (body "")
+              to subject
+              ;; In addition to To, Subject and Body these headers are
+              ;; allowed:
+              (allowed-xtra-hdrs '(cc bcc in-reply-to)))
+
+          (with-temp-buffer
+            ;; Extract body if it's defined
+            (when (assoc "Body" hdr-alist)
+              (dolist (hdr hdr-alist)
+                (when (equal "Body" (car hdr))
+                  (insert (format "%s\n" (cdr hdr)))))
+              (rfc2047-decode-region (point-min) (point-max))
+              (setq body (buffer-substring-no-properties
+                          (point-min) (point-max)))
+              (erase-buffer))
+
+            ;; Extract headers
+            (dolist (hdr hdr-alist)
+              (unless (equal "Body" (car hdr))
+                (insert (format "%s: %s\n" (car hdr) (cdr hdr)))))
+            (rfc2047-decode-region (point-min) (point-max))
+            (goto-char (point-min))
+            (setq hdr-alist (mail-header-extract-no-properties)))
+
+          (setq to (or (cdr (assq 'to hdr-alist)) "")
+                subject (or (cdr (assq 'subject hdr-alist)) "")
+                hdr-alist
+                (remove nil (mapcar
+                             #'(lambda (item)
+                                 (when (memq (car item) allowed-xtra-hdrs)
+                                   (cons (capitalize (symbol-name (car item)))
+                                         (cdr item))))
+                             hdr-alist)))
+
+          (compose-mail to subject hdr-alist nil nil
+                        (list (lambda (string)
+                                (insert string))
+                              body))))
+    (compose-mail)))
+
 ;; key bindings
 (global-set-key (kbd "<C-up>") 'shrink-window)
 (global-set-key (kbd "<C-down>") 'enlarge-window)
@@ -156,12 +215,12 @@
  '(custom-enabled-themes (quote (tango-dark)))
  '(read-file-name-completion-ignore-case t)
  '(vc-handled-backends nil)
- '(message-user-fqdn "linux.ibm.com")
  '(sendmail-program "/usr/bin/msmtp")
  '(send-mail-function 'sendmail-send-it)
  '(notmuch-fcc-dirs "Sent")
  '(notmuch-search-oldest-first nil)
  '(notmuch-mua-cite-function 'message-cite-original-without-signature)
+ '(frame-title-format "notmuch")
  '(awesome-tab-common-group-name "notmuch")
  '(awesome-tab-cycle-scope 'tabs)
  '(awesome-tab-display-sticky-function-name nil)
